3ab6a436783caefcd15d083882e8754440a8aaf0
trezz <iy.vangoethe@gmail.com>
2021-05-11 09:30:37 +0200

    [UPDATE] updated the backend to run on heroku

--- dist/src/app.js
+++ dist/src/app.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = void 0;
 const express_1 = __importDefault(require("express"));
-// const connectDB = require("../config/db");
-const db_1 = require("config/db");
+const connectDB = require("../config/db");
+// import { connectDB } from "config/db";
 const app = express_1.default();
 // connection to mongo atlas database
-db_1.connectDB();
+connectDB();
 // Init Middleware
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
